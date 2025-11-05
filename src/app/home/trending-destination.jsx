c6469270114a08e4728791d5e566b0b8a82e1d8a
Aicha ben slama <aichabenslama152@gmail.com>
2025-11-05 17:26:00 +0000

    feat : implement trending destination section

--- src/app/home/trending-destination.jsx
+++ src/app/home/trending-destination.jsx
@@ -1,8 +1,29 @@
+import { TRENDING_DESTINATIONS } from '@/config/app.config'
+import { getAsserPath } from '@/lib/utils'
 import React from 'react'
 
 const TrendingDestination = () => {
   return (
-    <div>TrendingDestination</div>
+    <div className='container my-16'>
+      <div className='mb-4 space-y-1'>
+        <h2 className='text-2xl font-bold'>Trending Destination</h2>
+        <p className='text-base text-muted-foreground'>Most popular choices for travellers from Tunisia</p>
+        <div className='grid grid-cols-6 gap-4'>
+          {TRENDING_DESTINATIONS.map((destination,index)=>(
+            <div key={index} className={`h-[270px] rounded-lg overflow-hidden relative ${destination.className}`} >
+              <img className='object-cover size-full rounded-lg' src={getAsserPath(destination.image)} alt={destination.title} />
+              <div>
+                <div>
+                  <h3>{destination.title}</h3>
+                </div>
+              </div>
+            </div>
+
+          ))}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
